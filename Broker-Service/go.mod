41152a6300609d61ed7840343d3b2d6eedd1a243
Jaycepticon <58182966+jayanthsharabu@users.noreply.github.com>
2025-04-28 09:00:35 +0530

    logger-service-v1

--- Broker-Service/go.mod
+++ Broker-Service/go.mod
@@ -3,7 +3,7 @@ module broker
 go 1.23.4
 
 require (
-	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-chi/chi v1.5.5 
+	github.com/go-chi/chi/v5 v5.2.1 
+	github.com/go-chi/cors v1.2.1 
 )
---
+++ Logger-service/cmd/api/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"Logger-service/data"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "8001"
+	mongoURL = "mongodb://mongo:27017"
+	grpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	//context
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	//cance
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	//server
+
+	go app.serve()
+
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting to mongo", err)
+		return nil, err
+	}
+
+	return c, nil
+}
